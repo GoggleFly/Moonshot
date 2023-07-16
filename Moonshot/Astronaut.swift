1a4f965e03b1566709732cce1733b24201513e9c
David Ash <david.ash@ottid.co.uk>
2023-07-16 21:02:43 +0100

    Baseline project complete

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by David Ash on 16/07/2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
